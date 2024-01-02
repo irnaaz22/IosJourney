819f204a85d34d74fc3cc5c66e0e383e7e0e6f0c
irnaaz22 <irnaaz.workmail@gmail.com>
2024-01-01 23:22:55 -0700

    Add existing file

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,16 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Tajdar unnisa Begum on 03/12/23.
+//
+
+import Foundation
+
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
+
